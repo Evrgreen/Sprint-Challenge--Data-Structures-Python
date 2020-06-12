0141f541e3e6735233db720b0bd961edaf668620
evrgreen <rcarsten06@gmail.com>
2020-06-12 09:24:49 -0600

    Finsished Ring Buffer excercise

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.storage = [None] * self.capacity
+        self.pointer = 0
 
     def append(self, item):
-        pass
+        self.storage[self.pointer] = item
+        self.pointer = (self.pointer + 1) % self.capacity
+        print(self.pointer)
 
     def get(self):
-        pass
+        return [item for item in self.storage if item is not None]
+
+
+test_buffer = RingBuffer(5)
+test_buffer.append(1)
+test_buffer.append(2)
+
+print(test_buffer.storage)
+print(test_buffer.get())
