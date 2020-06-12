a0495e97b343f266fd72843d0ec9fdae73d3c17f
evrgreen <rcarsten06@gmail.com>
2020-06-12 10:28:26 -0600

    finished reverse list

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -12,6 +12,10 @@ class Node:
     def set_next(self, new_next):
         self.next_node = new_next
 
+    def __str__(self):
+        return str(self.value)
+
+
 class LinkedList:
     def __init__(self):
         self.head = None
@@ -38,5 +42,33 @@ class LinkedList:
 
         return False
 
+    def __str__(self):
+        # current = self.head
+        # while current:
+        #     print(current)
+        #     current = current.next_node
+        # return str(self.head.value)
+        return f'{str(self.head.value)} {str(self.head.next_node.value)}{str(self.head.next_node.next_node.value)} {str(self.head.next_node.next_node.next_node.value)}'
+
     def reverse_list(self, node, prev):
-        pass
+        if prev is not None:
+            if not prev.next_node:
+                prev.set_next(node)
+                self.head = prev
+            else:
+                self.reverse_list(prev, prev.next_node)
+            prev.set_next(node)
+        elif node and node.next_node:
+            self.reverse_list(node, node.next_node)
+
+
+test_list = LinkedList()
+test_list.add_to_head(1)
+test_list.add_to_head(2)
+test_list.add_to_head(3)
+test_list.add_to_head(4)
+test_list.add_to_head(5)
+
+print(test_list)
+test_list.reverse_list(test_list.head, test_list.head.next_node)
+print(test_list)
